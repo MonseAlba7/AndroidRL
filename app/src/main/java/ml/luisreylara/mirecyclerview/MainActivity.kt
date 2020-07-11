6f2748b4a91a4a6ff3e2be0bb5ec2dda4e9f5ad3
luisreylarag <llara_ptc@upjr.edu.mx>
2020-07-11 15:29:39 -0500

    SABADO11072020

--- app/src/main/java/ml/luisreylara/mirecyclerview/MainActivity.kt
+++ app/src/main/java/ml/luisreylara/mirecyclerview/MainActivity.kt
@@ -2,7 +2,7 @@ package ml.luisreylara.mirecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//ejemplo
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
